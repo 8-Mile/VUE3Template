c9fdffe2f044a76f64277afd3e74d22a91593cd7
xiaweidong <xiaweidong@hasng.cn>
2022-03-30 17:32:00 +0800

    提交文件

---
+++ src/views/main/dashboard/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <Card />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from './components/card/index.vue'
+export default defineComponent({
+  components: {
+    Card,
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+  .box {
+    padding: 15px;
+  }
+</style>
